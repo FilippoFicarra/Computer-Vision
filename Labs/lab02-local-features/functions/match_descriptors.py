02439bc6c34aa571e6fbeed8cda938486690e538
FilippoFicarra <82835577+FilippoFicarra@users.noreply.github.com>
2023-10-04 11:50:43 +0200

    finished lab2

--- Labs/lab02-local-features/functions/match_descriptors.py
+++ Labs/lab02-local-features/functions/match_descriptors.py
@@ -25,22 +25,42 @@ def match_descriptors(desc1, desc2, method = "one_way", ratio_thresh=0.5):
     '''
     assert desc1.shape[1] == desc2.shape[1]
     distances = ssd(desc1, desc2)
-    print(distances.shape)
     q1, q2 = desc1.shape[0], desc2.shape[0]
     matches = None
     if method == "one_way": # Query the nearest neighbor for each keypoint in image 1
         # TODO: implement the one-way nearest neighbor matching here
         # You may refer to np.argmin to find the index of the minimum over any axis
-        raise NotImplementedError
+        matches = np.argmin(distances, axis=1)
+        matches = np.array([(i, match) for i, match in enumerate(matches)])
     elif method == "mutual":
         # TODO: implement the mutual nearest neighbor matching here
         # You may refer to np.min to find the minimum over any axis
-        raise NotImplementedError
+        matches_1 = np.argmin(distances, axis=1)
+        matches_1 = np.array([(i, match) for i, match in enumerate(matches_1)])
+
+        matches_2 = np.argmin(distances, axis=0)
+        matches_2 = np.array([(i, match) for i, match in enumerate(matches_2)])
+
+        matches_2 = matches_2[:, [1, 0]]
+
+        matches_1_set = set([tuple((match[0], match[1])) for match in matches_1])
+        matches_2_set = set([tuple((match[0], match[1])) for match in matches_2])
+
+        matches = list(matches_1_set.intersection(matches_2_set))
+        matches = np.array([[match[0], match[1]] for match in matches])
+
     elif method == "ratio":
         # TODO: implement the ratio test matching here
         # You may use np.partition(distances,2,axis=0)[:,1] to find the second smallest value over a row
-        raise NotImplementedError
+        matches = np.argmin(distances, axis=1)
+        matches = np.array([(i, match) for i, match in enumerate(matches)])
+
+        partition_first= np.partition(distances,1,axis=1)[:, 0]
+
+        partition_second = np.partition(distances,1,axis=1)[:, 1]
+
+        matches = matches[partition_first/partition_second < ratio_thresh ]
     else:
-        raise NotImplementedError
+        matches = np.array([[]])
     return matches
 
